369e392c2609c8bfa79497afb4d9599c554c4c9d
Mathu-lmn <mathurapha@gmail.com>
2022-05-03 15:43:53 +0200

    Init

---
+++ c-function.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+void optimal_maintenance_route() {
+    
+}
---
+++ main.py
@@ -0,0 +1,51 @@
+from tokenize import Name
+
+
+class Bikes:
+    UID = None
+    Battery = 0
+    Nb_days = 0
+    Nb_rents = 0
+
+class Stations:
+    UID = None
+    Name = None
+    Location = []
+    Bikes = []
+    Nb_rents = 0
+    Nb_returns = 0
+
+def import_bikes_and_station(filename):
+    """
+    Import all the bikes and stations from the file filename.
+    """
+    # TODO
+    pass
+
+def dock_bike(station, bike):
+    """
+    Dock a bike at a station.
+    """
+    # TODO
+    pass
+
+def display_stations_and_bikes():
+    """
+    Display all stations and bikes.
+    """
+    # TODO
+    pass
+
+def rent_bike(station, bike):
+    """
+    Rent a bike from a station.
+    """
+    # TODO
+    pass
+
+def maintenance_defective_bikes():
+    """
+    Maintenance of all the defective bikes.
+    """
+    # TODO
+    pass
