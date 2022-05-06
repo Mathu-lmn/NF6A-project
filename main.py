d7542991a9dd455996257d4c3b9b16b56e0196f6
Mathu-lmn <mathurapha@gmail.com>
2022-05-06 20:09:05 +0200

    Done display_stations function

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import csv
+from tempfile import tempdir
 import pandas as pd
 
 
@@ -11,7 +12,7 @@ class Bikes:
 data = list(csv.reader(open('Bikes.csv'), delimiter=','))
 bikes = [Bikes(i, data[0]) for i in data[1:]]
 
-print(bikes[2].UID)
+#print(bikes[2].UID)
 
 
 class Stations:
@@ -22,8 +23,6 @@ class Stations:
 
 data = list(csv.reader(open('Stations.csv'), delimiter=','))
 stations = [Stations(i, data[0]) for i in data[1:]]
-
-print(stations[3].Location)
 
 
 def dock_bike():
@@ -45,22 +44,41 @@ def dock_bike():
     df.to_csv('Stations.csv', index=False)
 
 # print(pd.read_csv('Stations.csv', sep=','))
-df = pd.read_csv('Stations.csv', sep=',')
-dock_bike()
+#df = pd.read_csv('Stations.csv', sep=',')
+#dock_bike()
 
-def display_stations_and_bikes():
+def display_stations():
     """
-    Display all stations and bikes.
+    Display all stations and the bikes docked to it in a descending order according to their battery level.
     """
-    # TODO
-    pass
+    print('Stations\nUID\tLocation\tBikes')
+    for x in range(len(stations)):
+        i = stations[x].Bikes.split(',')
+        if len(i) == 1:
+            print(f"{stations[x].UID}\t {stations[x].Location}\t {stations[x].Bikes}")
+        else:
+            for y in range(len(i)-1):
+                for j in range(0,len(i)-y-1):
+                    #print(f"in for loop {i} and j is {j}, bike {i[j]} has a battery level of {bikes[int(i[j])-1].battery_percent}, bike {i[j+1]} has a battery level of {bikes[int(i[j+1])-1].battery_percent}")
+                    if int(bikes[int(i[j])-1].battery_percent) < int(bikes[int(i[j+1])-1].battery_percent):
+                        k = i[j+1]
+                        i[j+1] = bikes[int(i[j])-1].UID
+                        i[j] = bikes[int(k)-1].UID
+                    else:
+                        pass
+            print(f"{stations[x].UID}\t {stations[x].Location}\t {','.join(i)}")
+
+#display_stations()
+    
 
 def rent_bike(station, bike):
     """
-    Rent a bike from a station.
+    User will rent a bike from a station, will return it after x minutes to another or the same station.
     """
-    # TODO
-    pass
+    bike = input('Enter bike UID: ')
+    arrival_station = input('Enter station UID: ')
+    rental_time = input('Enter time of rental: ')
+    
 
 def maintenance_defective_bikes():
     """
